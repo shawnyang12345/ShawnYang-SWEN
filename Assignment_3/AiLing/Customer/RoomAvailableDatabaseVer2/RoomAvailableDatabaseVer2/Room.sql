e29c140bf771b35e2466ea8d2743463f889e4dc4
shawnyang12345 <shawnyangxw@hotmail.com>
2017-06-06 12:54:18 +0800

    Add files via upload

---
+++ Assignment_3/AiLing/Customer/RoomAvailableDatabaseVer2/RoomAvailableDatabaseVer2/Room.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Room]
+(
+	[roomId] INT IDENTITY (1, 1) NOT NULL ,
+	[roomType] VARCHAR(50) NOT NULL,
+	[roomRate] DECIMAL(18,0) NOT NULL,
+	[status] VARCHAR(50) NOT NULL,
+	[isSmoking] VARCHAR(50) NOT NULL,
+	PRIMARY KEY CLUSTERED ([roomId] ASC)
+)
