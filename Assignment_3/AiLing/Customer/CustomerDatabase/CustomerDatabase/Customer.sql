88042a9ddb2f714e1c1bd4f5d1393cd2634bd9d2
shawnyang12345 <shawnyangxw@hotmail.com>
2017-06-06 12:51:32 +0800

    Add files via upload

---
+++ Assignment_3/AiLing/Customer/CustomerDatabase/CustomerDatabase/Customer.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[customerId] INT IDENTITY (1, 1) NOT NULL ,
+	[firstName] VARCHAR(50) NOT NULL,
+	[lastName] VARCHAR(50) NOT NULL,
+	[email] VARCHAR(50) NULL,
+	[address] VARCHAR(50) NOT NULL,
+	[city] VARCHAR(50) NULL,
+	[country] VARCHAR(50) NOT NULL,
+	[postalCode] INT NOT NULL,
+	[contactNum] VARCHAR(50) NOT NULL,
+	PRIMARY KEY CLUSTERED ([customerId] ASC)
+)
